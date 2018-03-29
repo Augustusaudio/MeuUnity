93007c29d42188ae85b1d5fafa8bc68605f35e25
FELIPE\Silva <f.augustus@gmail.com>
2018-03-29 18:49:24 -0300

    Aceleração do foguete funciona

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+	
+	
+	Rigidbody rigidBody;
 	// Use this for initialization
 	void Start () {
-		
+		rigidBody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -17,7 +20,7 @@ public class Rocket : MonoBehaviour {
 
     private void ProcessInput()
     {
-	if (Input.GetKey(KeyCode.Space))	{print("Thrust");} 
+	if (Input.GetKey(KeyCode.Space))	{rigidBody.AddRelativeForce(Vector3.up);} 
 	//can thrust while rotating
 	
 	{if (Input.GetKey(KeyCode.A))	    {print("Rotating Left");}
