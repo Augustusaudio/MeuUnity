5eb47201c35958b71349cd8cffbd3dfc966471e2
FELIPE\Silva <f.augustus@gmail.com>
2018-04-02 16:59:59 -0300

    Efeito sonoro no foguete: código funciona, o áudio é uma bosta. Trocar

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,11 +7,13 @@ public class Rocket : MonoBehaviour {
 
 	
 	
+
 	Rigidbody rigidBody;
-	
+	AudioSource audioSource;
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent <AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -21,7 +23,16 @@ public class Rocket : MonoBehaviour {
 
     private void ProcessInput()
     {
-	if (Input.GetKey(KeyCode.Space))	{rigidBody.AddRelativeForce(Vector3.up);} 
+	if (Input.GetKey(KeyCode.Space))	{
+		rigidBody.AddRelativeForce(Vector3.up);
+		if (!audioSource.isPlaying) //so it doesn't layer
+		{audioSource.Play();}
+		}
+		else
+		{audioSource.Stop();}
+	
+	
+	
 	//can thrust while rotating
 	
 	{if (Input.GetKey(KeyCode.A))	    {transform.Rotate(Vector3.forward);}
